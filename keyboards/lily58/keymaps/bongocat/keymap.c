e4673b6341ff82f4dbca1e53ea08157b16501ac7
Stefano Bertolotto <stefano.bertolotto@miniclip.com>
2022-02-09 09:33:00 +0100

    First version of symbol layout

--- keyboards/lily58/keymaps/bongocat/keymap.c
+++ keyboards/lily58/keymaps/bongocat/keymap.c
@@ -111,11 +111,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______,      _______, _______, _______, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP, KC_END,  _______, \
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,		KC_RGHT,	XXXXXXX, XXXXXXX, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,       KC_LBRC,    KC_RBRC, KC_BSLS, \
-                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______,                  	_______, _______, _______,	_______,	_______, 	_______, \
+  _______, KC_LT,   KC_DQT,  KC_GT,   KC_COLN, _______,                  	_______, KC_HOME, KC_PGDN,	KC_PGUP, 	KC_END, 	_______, \
+  _______, KC_EQL,  KC_MINS, KC_LPRN, KC_RPRN, _______,                  	_______, KC_LEFT, KC_DOWN,	KC_UP,		KC_RGHT,	XXXXXXX, \
+  _______, KC_COLN, KC_HASH, KC_LCBR, KC_RCBR, _______,   _______, _______,	_______, _______, _______,	_______,	_______,	_______, \
+                             _______, _______, _______,  _______, _______,	_______, _______, 		_______ \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
