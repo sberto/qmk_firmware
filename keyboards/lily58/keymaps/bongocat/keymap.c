6bf493b64da8366a4bb4a362bd5f1070ffcf2f1c
Stefano Bertolotto <stefano.bertolotto@miniclip.com>
2022-02-15 15:14:38 +0100

    moved caps lock to ctrl

--- keyboards/lily58/keymaps/bongocat/keymap.c
+++ keyboards/lily58/keymaps/bongocat/keymap.c
@@ -112,7 +112,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_U]              = ACTION_TAP_DANCE_FN_ADVANCED(NULL, acc_u, stop_u),
 };
 
-#define CAPS_SHIFT MT(MOD_LSFT, KC_CAPS)
+#define CAPS_CTRL MT(MOD_LCTL, KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -134,15 +134,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
 	KC_ESC,		KC_1,		KC_2,	KC_3,		KC_4,	KC_5,								KC_6,	KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,\
 	KC_TAB,		KC_Q,		KC_W,	TD(TD_E),	KC_R,	KC_T,								KC_Y,	TD(TD_U),	TD(TD_I),	TD(TD_O),	KC_P,		KC_EQL, \
-	KC_LCTRL,	TD(TD_A),	KC_S,	KC_D,		KC_F,	KC_G,								KC_H,	KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT, \
-	CAPS_SHIFT,	KC_Z,		KC_X,	KC_C,		KC_V,	KC_B,		KC_LBRC,	KC_RBRC,	KC_N,	KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_BSLS,	\
+	CAPS_CTRL,  TD(TD_A),	KC_S,	KC_D,		KC_F,	KC_G,								KC_H,	KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT, \
+	KC_LSFT,	KC_Z,		KC_X,	KC_C,		KC_V,	KC_B,		KC_LBRC,	KC_RBRC,	KC_N,	KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_BSLS,	\
     			KC_LALT,	KC_LGUI,	TD(TD_QWERTY_COLEMAK),	KC_SPC,		KC_ENT,		MO(_RAISE),	KC_BSPC,	KC_GRV\
 ),
  [_COLEMAK] = LAYOUT( \
 	KC_ESC,		KC_1,		KC_2,	KC_3,	KC_4,	KC_5,                    			KC_6,	KC_7,	KC_8,		KC_9,		KC_0,		KC_MINS, \
 	KC_TAB,		KC_Q,		KC_W,	KC_F,	KC_P,	KC_B,                    			KC_J,	KC_L,	TD(TD_U),	KC_Y,		KC_SCLN,	KC_EQL, \
-	KC_LCTRL,	TD(TD_A),	KC_R,	KC_S,	KC_T,	KC_G,                   			KC_M,	KC_N,	TD(TD_E),	TD(TD_I),	TD(TD_O),		KC_QUOT, \
-	CAPS_SHIFT,	KC_Z,		KC_X,	KC_C,	KC_D,	KC_V,		KC_LBRC,	KC_RBRC,	KC_K,	KC_H,	KC_COMM,	KC_DOT,		KC_SLSH,  KC_BSLS,	\
+	CAPS_CTRL,	TD(TD_A),	KC_R,	KC_S,	KC_T,	KC_G,                   			KC_M,	KC_N,	TD(TD_E),	TD(TD_I),	TD(TD_O),		KC_QUOT, \
+	KC_LSFT,	KC_Z,		KC_X,	KC_C,	KC_D,	KC_V,		KC_LBRC,	KC_RBRC,	KC_K,	KC_H,	KC_COMM,	KC_DOT,		KC_SLSH,  KC_BSLS,	\
 				KC_LALT,	KC_LGUI,	TD(TD_QWERTY_COLEMAK),	KC_SPC,		KC_ENT,		MO(_RAISE),	KC_BSPC,	KC_GRV \
 ),
 /* LOWER
