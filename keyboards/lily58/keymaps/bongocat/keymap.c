c8f80480207d85651b87657010e2a7192b85c704
Stefano Bertolotto <stefano.bertolotto@miniclip.com>
2022-03-29 14:28:01 +0200

    Changed = to shortcut for format code on intellij

--- keyboards/lily58/keymaps/bongocat/keymap.c
+++ keyboards/lily58/keymaps/bongocat/keymap.c
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_COLEMAK] = LAYOUT( \
 	KC_ESC,		KC_1,		KC_2,	KC_3,	KC_4,	KC_5,                    			KC_6,	KC_7,	KC_8,		KC_9,		KC_0,		KC_MINS, \
-	KC_TAB,		KC_Q,		KC_W,	KC_F,	KC_P,	KC_B,                    			KC_J,	KC_L,	TD(TD_U),	KC_Y,		KC_SCLN,	KC_EQL, \
+	KC_TAB,		KC_Q,		KC_W,	KC_F,	KC_P,	KC_B,                    			KC_J,	KC_L,	TD(TD_U),	KC_Y,		KC_SCLN,	LAG(KC_L), \
 	CAPS_CTRL,	TD(TD_A),	KC_R,	KC_S,	KC_T,	KC_G,                   			KC_M,	KC_N,	TD(TD_E),	TD(TD_I),	TD(TD_O),		KC_QUOT, \
 	KC_LSFT,	KC_Z,		KC_X,	KC_C,	KC_D,	KC_V,		KC_LBRC,	KC_RBRC,	KC_K,	KC_H,	KC_COMM,	KC_DOT,		KC_SLSH,  KC_BSLS,	\
 				KC_LALT,	KC_LGUI,	TD(TD_QWERTY_COLEMAK),	KC_SPC,		KC_ENT,		MO(_RAISE),	TD(TD_DEL),	KC_GRV \
