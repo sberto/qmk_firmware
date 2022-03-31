adec5f4ce1cc7ede88206b64b3bdee7b18899ef9
Stefano Bertolotto <stefano.bertolotto@miniclip.com>
2022-03-31 10:40:55 +0200

    Removed tap-dance layer for faster switching

--- keyboards/lily58/keymaps/bongocat/keymap.c
+++ keyboards/lily58/keymaps/bongocat/keymap.c
@@ -17,34 +17,13 @@ enum custom_keycodes {
 };
 
 enum {
-  TD_QWERTY_COLEMAK = 0,
-  TD_A,
+  TD_A = 0,
   TD_E,
   TD_I,
   TD_O,
   TD_U,
   TD_DEL,
 };
-
-void dance_colemak(qk_tap_dance_state_t* state, void* user_data)
-{
-    if (state->count == 2)
-    {
-        layer_invert(_COLEMAK);
-        layer_invert(_QWERTY);
-    }
-    else
-    {
-        if (state->pressed) {
-            layer_on(_LOWER);
-        }
-    }
-}
-
-void dance_no_lower(qk_tap_dance_state_t* state, void* user_data)
-{
-    layer_off(_LOWER);
-}
 
 void acc4(int code, char* uc, char* sh_uc, char* uc2, char* sh_uc2, qk_tap_dance_state_t *state) {
     if (state->count >= 3) {
@@ -106,7 +85,6 @@ void stop_u(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_QWERTY_COLEMAK] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_colemak, dance_no_lower),
     [TD_A]              = ACTION_TAP_DANCE_FN_ADVANCED(NULL, acc_a, stop_a),
     [TD_E]              = ACTION_TAP_DANCE_FN_ADVANCED(NULL, acc_e, stop_e),
     [TD_I]              = ACTION_TAP_DANCE_FN_ADVANCED(NULL, acc_i, stop_i),
@@ -138,14 +116,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,		KC_Q,		KC_W,	KC_F,	KC_P,	KC_B,                    			KC_J,	KC_L,	TD(TD_U),	KC_Y,		KC_SCLN,	LAG(KC_L), \
 	CAPS_CTRL,	TD(TD_A),	KC_R,	KC_S,	KC_T,	KC_G,                   			KC_M,	KC_N,	TD(TD_E),	TD(TD_I),	TD(TD_O),		KC_QUOT, \
 	KC_LSFT,	KC_Z,		KC_X,	KC_C,	KC_D,	KC_V,		KC_LBRC,	KC_RBRC,	KC_K,	KC_H,	KC_COMM,	KC_DOT,		KC_SLSH,  KC_BSLS,	\
-				KC_LALT,	KC_LGUI,	TD(TD_QWERTY_COLEMAK),	KC_SPC,		KC_ENT,		MO(_RAISE),	TD(TD_DEL),	KC_GRV \
+				KC_LALT,	KC_LGUI,	MO(_LOWER),	KC_SPC,		KC_ENT,		MO(_RAISE),	TD(TD_DEL),	KC_GRV \
 ),
  [_QWERTY] = LAYOUT( \
 	KC_ESC,		KC_1,		KC_2,	KC_3,		KC_4,	KC_5,								KC_6,	KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,\
 	KC_TAB,		KC_Q,		KC_W,	TD(TD_E),	KC_R,	KC_T,								KC_Y,	TD(TD_U),	TD(TD_I),	TD(TD_O),	KC_P,		KC_EQL, \
 	CAPS_CTRL,  TD(TD_A),	KC_S,	KC_D,		KC_F,	KC_G,								KC_H,	KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT, \
 	KC_LSFT,	KC_Z,		KC_X,	KC_C,		KC_V,	KC_B,		KC_LBRC,	KC_RBRC,	KC_N,	KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_BSLS,	\
-    			KC_LALT,	KC_LGUI,	TD(TD_QWERTY_COLEMAK),	KC_SPC,		KC_ENT,		MO(_RAISE),	TD(TD_DEL),	KC_GRV\
+    			KC_LALT,	KC_LGUI,	MO(_LOWER),	KC_SPC,		KC_ENT,		MO(_RAISE),	TD(TD_DEL),	KC_GRV\
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -209,7 +187,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, MEH(KC_A), MEH(KC_S), MEH(KC_D), XXXXXXX,               XXXXXXX, MEH(KC_H), MEH(KC_J), MEH(KC_K),  MEH(KC_L), XXXXXXX, \
   XXXXXXX, XXXXXXX, LSA(KC_A), LSA(KC_S), LSA(KC_D), XXXXXXX, 				XXXXXXX, LSA(KC_H), LSA(KC_J), LSA(KC_K),  LSA(KC_L), XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+                             DF(_COLEMAK), _______, _______, _______, _______,  _______, _______, DF(_QWERTY) \
   )
 };
 
